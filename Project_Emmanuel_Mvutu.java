43178ecef6aa07e732efca0b3916d40efeeff4e3
Emmanuel Mvutu <emvutu00@email.cpcc.edu>
2021-03-06 16:49:01 -0500

    Project 1 second update
    Added code for determining performance score and recommended graphics quality

--- Project_Emmanuel_Mvutu.java
+++ Project_Emmanuel_Mvutu.java
@@ -4,9 +4,19 @@ public class Project_Emmanuel_Mvutu
 {
    public static void main(String[] args)
    {
-      int GPUClockSpeed = 0;//to hold the GPU clock speed
-      int CPUClockSpeed = 0;//to hold the CPU clock speed
-      int numOfCores = 0;//to hold the number of cores
+      int GPUClockSpeed = 0;//to hold the place of GPU clock speed
+      int CPUClockSpeed = 0;//to hold the place of CPU clock speed
+      int numOfCores = 0;//to hold the place of the number of cores
+      int resolution = 0;//to hold the place of resolution
+      double multiplier = 0;//to hold the place of resolution multiplier
+      double performanceScore = 0;//to hold the place of performance score
+      String recommendedGraphicQuality;
+      
+      //represents the resolution options
+      final int RESOLUTION_1 = 1;
+      final int RESOLUTION_2 = 2;
+      final int RESOLUTION_3 = 3;
+      final int RESOLUTION_4 = 4;
       
       Scanner keyboard = new Scanner(System.in);//creating a Scanner object for  the user's input
       
@@ -29,6 +39,52 @@ public class Project_Emmanuel_Mvutu
       System.out.println("3. 2560 x 1440");
       System.out.println("4. 3840 x 2160");
       
+      //ask the user to select their resolution
+      System.out.print("\nPlease select your resolution from above: ");
+      resolution = keyboard.nextInt();
+      
+      //using an if/else if statement to handle the user's resolution selection
+      if(resolution == RESOLUTION_1)
+         {
+            multiplier = 1;
+         }
+      else if(resolution == RESOLUTION_2)
+         {
+            multiplier = .75;
+         }
+      else if(resolution == RESOLUTION_3)
+         {
+            multiplier = .55;
+         }
+      else
+         {
+            multiplier = .35;
+         }
+         
+      //calculate the performance score from the given inputs from the user
+      performanceScore = ((5 * GPUClockSpeed) + (numOfCores * CPUClockSpeed)) * multiplier;
+      
+      //
+      if(performanceScore >= 17000)
+         {
+            recommendedGraphicQuality = "Ultra";
+         }
+      else if(performanceScore >= 15000 && performanceScore <= 17000)
+         {
+            recommendedGraphicQuality = "High";
+         }
+      else if(performanceScore >= 13000 && performanceScore <= 15000)
+         {
+            recommendedGraphicQuality = "Medium";
+         }
+      else if(performanceScore >= 11000 && performanceScore <= 13000)
+         {
+            recommendedGraphicQuality = "Low";
+         }
+      else
+         {
+            recommendedGraphicQuality = "Unable to play";
+         }
       
    }
 }
